0a82b4dd074f5d2789abf48db936a63f3d54a11f
Vitaly01 <sashaomg@hotmail.com>
2015-07-06 22:46:01 -0400

    Add .sh file

---
+++ xtract.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SLACKBUILD="$1"
+SOURCE="$2"
+SLACKBUILDNAME="$(basename $SLACKBUILD .tar.gz)"
+
+if [ -z $SOURCE]; then
+	echo "$0 Slackbuild Source"
+	exit 
+fi
+
+if [ ! -f $PWD/$SLACKBUILD ]; then
+    echo "Error: $SLACKBUILD not exist"
+    exit
+fi
+
+if [ ! -f $PWD/$SOURCE ]; then
+    echo "Error: $SOURCE not exist"
+    exit
+fi
+
+
+gzip -d $SLACKBUILD
+tar -xf $SLACKBUILDNAME.tar
+rm $SLACKBUILDNAME.tar
+mv $SOURCE $SLACKBUILDNAME
